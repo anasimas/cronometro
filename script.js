381ebe9da567010d444fb848011903c369d0458f
anasimas <ana.simas2109@outlook.com>
2023-04-05 15:03:05 -0300

    feat: ajustes no timer

--- script.js
+++ script.js
@@ -1,63 +1,73 @@
-let botaoPlay = document.getElementById("play");
-let botaoPause = document.getElementById("pause");
-let botaoReset = document.getElementById("reset");
+let botaoPlay = document.getElementById('play');
+let botaoPause = document.getElementById('pause');
+let botaoReset = document.getElementById('reset');
 
-let hora = document.getElementById("hora");
-let min = document.getElementById("min");
-let seg = document.getElementById("seg");
+let hora = document.getElementById('hora');
+let min = document.getElementById('min');
+let seg = document.getElementById('seg');
 
-function play(){
+let segAtual = 0;
+let minAtual = 0;
+let horaAtual = 0;
+
+function play() {
     var intervalo = setInterval(cronometro, 1000);
     botaoPlay.disabled = true;
     botaoPause.disabled = false;
     botaoReset.disabled = false;
+
+    botaoPause.addEventListener
 }
 
-function pause(){
-
+function pause() {
+    clearInterval(intervalo);
     botaoPlay.disabled = false;
     botaoPause.disabled = true;
     botaoReset.disabled = false;
 }
 
-function reset(){
+function reset() {
     clearInterval(intervalo);
     botaoPlay.disabled = false;
     botaoPause.disabled = true;
     botaoReset.disabled = true;
+
+    seg.innerHTML = "00";
+    min.innerHTML = "00";
+    hora.innerHTML = "00";
+
+    segAtual = 0;
+    minAtual = 0;
+    horaAtual = 0;
 }
 
-function cronometro(){
-    var segAtual = 0;
-    var minAtual = 0;
-    var horaAtual = 0;
-
-    if(segAtual <= 60){
-        if(segAtual < 9){
+function cronometro() {
+    if (segAtual <= 60) {
+        if (segAtual < 9) {
             segAtual++;
             seg.innerHTML = "0" + segAtual;
-        }else{
+        } else {
             segAtual++;
             seg.innerHTML = segAtual;
         }
 
-        if(segAtual === 60){
+        if (segAtual === 60) {
             segAtual = 0;
             seg.innerHTML = "00";
-            if(minAtual <= 60){
-                if(minAtual < 9){
+            if (minAtual <= 60) {
+                if (minAtual < 9) {
                     minAtual++;
                     min.innerHTML = "0" + minAtual;
-                }else{
+                } else {
                     minAtual++;
                     min.innerHTML = minAtual;
                 }
 
-                if(minAtual === 60){
+                if (minAtual === 60) {
                     minAtual = 0;
                     min.innerHTML = "00";
 
-                    if(horaAtual < 9){
+                    if (horaAtual < 9) {
                         hora.innerHTML = "0" + horaAtual;
                     }
                 }
