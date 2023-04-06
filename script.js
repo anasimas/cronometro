e86d522addc7685d791ac29587b1d5ef10c734f6
anasimas <ana.simas2109@outlook.com>
2023-04-06 18:40:41 -0300

    feat: introdução do pomodoro, necessário corrigir o js para alterar a formatação do cronometro após escolher as opções diferentes no menu

--- script.js
+++ script.js
@@ -1,77 +1,106 @@
-let botaoPlay = document.getElementById('play');
-let botaoPause = document.getElementById('pause');
-let botaoReset = document.getElementById('reset');
+document.addEventListener('DOMContentLoaded', () => {
+    let botaoNormal = document.getElementById('seletorNormal');
+    let botaoPomodoro = document.getElementById('seletorPomodoro');
+    let botaoVoltar = document.querySelectorAll('.botaoVoltar');
+    let elementoSeletor = document.getElementById('seletorTipoCronometro');
+    let elementoCronometroNormal = document.getElementById('cronometroNormal');
+    let elementoPomodoro = document.getElementById('cronometroPomodoro');
 
-let hora = document.getElementById('hora');
-let min = document.getElementById('min');
-let seg = document.getElementById('seg');
+    // funções referentes ao seletor do tipo de cronometro
+    botaoNormal.addEventListener("click", () => {
+        elementoSeletor.style.display = "none";
+        elementoCronometroNormal.style.display = "flex";
+    });
 
-let segAtual = 0;
-let minAtual = 0;
-let horaAtual = 0;
+    botaoPomodoro.addEventListener("click", () => {
+        elementoSeletor.style.display = "none";
+        elementoPomodoro.style.display = "flex";
+    })
 
-function play() {
-    var intervalo = setInterval(cronometro, 1000);
-    botaoPlay.disabled = true;
-    botaoPause.disabled = false;
-    botaoReset.disabled = false;
+    botaoVoltar.forEach(botaoVoltar => {
+        botaoVoltar.addEventListener("click", () => {
+            elementoSeletor.style.display = "grid";
+            elementoCronometroNormal.style.display = "none";
+            elementoPomodoro.style.display = "none";
+        })
+    });
 
-    botaoPause.addEventListener
-}
+    // funções relacionadas ao pomodoro
 
-function pause() {
-    clearInterval(intervalo);
-    botaoPlay.disabled = false;
-    botaoPause.disabled = true;
-    botaoReset.disabled = false;
-}
+    // funções referentes ao cronometro normal
 
-function reset() {
-    clearInterval(intervalo);
-    botaoPlay.disabled = false;
-    botaoPause.disabled = true;
-    botaoReset.disabled = true;
+    let botaoPlay = document.querySelectorAll('.play');
+    let botaoPause = document.querySelectorAll('.pause');
+    let botaoReset = document.querySelectorAll('.reset');
 
-    seg.innerHTML = "00";
-    min.innerHTML = "00";
-    hora.innerHTML = "00";
+    let hora = document.getElementsByClassName('hora');
+    let min = document.getElementsByClassName('min');
+    let seg = document.getElementsByClassName('seg');
 
-    segAtual = 0;
-    minAtual = 0;
-    horaAtual = 0;
-}
+    let segAtual = 0;
+    let minAtual = 0;
+    let horaAtual = 0;
 
-function cronometro() {
-    if (segAtual <= 60) {
-        if (segAtual < 9) {
-            segAtual++;
-            seg.innerHTML = "0" + segAtual;
-        } else {
-            segAtual++;
-            seg.innerHTML = segAtual;
-        }
+    botaoPlay.addEventListener("click", () => {
+        var intervalo = setInterval(cronometro, 1000);
+        botaoPlay.disabled = true;
+        botaoPause.disabled = false;
+        botaoReset.disabled = false;
 
-        if (segAtual === 60) {
-            segAtual = 0;
+        botaoPause.addEventListener("click", () => {
+            clearInterval(intervalo);
+            botaoPlay.disabled = false;
+            botaoPause.disabled = true;
+            botaoReset.disabled = false;
+        });
+        botaoReset.addEventListener("click", () => {
+            clearInterval(intervalo);
+            botaoPlay.disabled = false;
+            botaoPause.disabled = true;
+            botaoReset.disabled = true;
+
             seg.innerHTML = "00";
-            if (minAtual <= 60) {
-                if (minAtual < 9) {
-                    minAtual++;
-                    min.innerHTML = "0" + minAtual;
-                } else {
-                    minAtual++;
-                    min.innerHTML = minAtual;
-                }
+            min.innerHTML = "00";
+            hora.innerHTML = "00";
 
-                if (minAtual === 60) {
-                    minAtual = 0;
-                    min.innerHTML = "00";
+            segAtual = 0;
+            minAtual = 0;
+            horaAtual = 0;
+        });
+    })
 
-                    if (horaAtual < 9) {
-                        hora.innerHTML = "0" + horaAtual;
+    function cronometro() {
+        if (segAtual <= 60) {
+            if (segAtual < 9) {
+                segAtual++;
+                seg.innerHTML = "0" + segAtual;
+            } else {
+                segAtual++;
+                seg.innerHTML = segAtual;
+            }
+
+            if (segAtual === 60) {
+                segAtual = 0;
+                seg.innerHTML = "00";
+                if (minAtual <= 60) {
+                    if (minAtual < 9) {
+                        minAtual++;
+                        min.innerHTML = "0" + minAtual;
+                    } else {
+                        minAtual++;
+                        min.innerHTML = minAtual;
+                    }
+
+                    if (minAtual === 60) {
+                        minAtual = 0;
+                        min.innerHTML = "00";
+
+                        if (horaAtual < 9) {
+                            hora.innerHTML = "0" + horaAtual;
+                        }
                     }
                 }
             }
         }
     }
-}
+});
