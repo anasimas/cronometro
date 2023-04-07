5474345fb95bd59596fb7f854bf3d21c0f3dae5d
anasimas <ana.simas2109@outlook.com>
2023-04-07 12:26:43 -0300

    fix: correção na função pomodoro. Falta corrigir o break

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
     let botaoNormal = document.getElementById('seletorNormal');
     let botaoPomodoro = document.getElementById('seletorPomodoro');
-    let botaoVoltar = document.querySelectorAll('.botaoVoltar');
+    let botaoVoltar = document.getElementById('botaoVoltar');
     let elementoSeletor = document.getElementById('seletorTipoCronometro');
-    let elementoCronometroNormal = document.getElementById('cronometroNormal');
-    let elementoPomodoro = document.getElementById('cronometroPomodoro');
+    let elementoCronometro = document.getElementById('cronometro');
+    let cronometroSelecionado = '';
+    let pomodoroSelecionado = '';
+    let intervalo;
 
     // funções referentes ao seletor do tipo de cronometro
     botaoNormal.addEventListener("click", () => {
         elementoSeletor.style.display = "none";
-        elementoCronometroNormal.style.display = "flex";
+        elementoCronometro.style.display = "flex";
+        cronometroSelecionado = 'normal';
+        seg.innerHTML = "00";
+        min.innerHTML = "00";
+        hora.innerHTML = "00";
     });
 
     botaoPomodoro.addEventListener("click", () => {
         elementoSeletor.style.display = "none";
-        elementoPomodoro.style.display = "flex";
-    })
+        elementoCronometro.style.display = "flex";
+        cronometroSelecionado = 'pomodoro';
 
-    botaoVoltar.forEach(botaoVoltar => {
-        botaoVoltar.addEventListener("click", () => {
-            elementoSeletor.style.display = "grid";
-            elementoCronometroNormal.style.display = "none";
-            elementoPomodoro.style.display = "none";
-        })
+        pomodoroSelecionado = 'descanso';
+      minAtual = 4;
+        segAtual = 5
+        seg.innerHTML = "05";
+        min.innerHTML = "04";
+        hora.innerHTML = "00";
+        
+        // minAtual = 25;
+        // segAtual = 0
+        // seg.innerHTML = "00";
+        // min.innerHTML = "25";
+        // hora.innerHTML = "00";
+    });
+
+    botaoVoltar.addEventListener("click", () => {
+        elementoSeletor.style.display = "grid";
+        elementoCronometro.style.display = "none";
+        clearInterval(intervalo);
+        botaoPlay.disabled = false;
+        botaoPause.disabled = true;
+        botaoReset.disabled = true;
     });
 
     // funções relacionadas ao pomodoro
 
     // funções referentes ao cronometro normal
 
-    let botaoPlay = document.querySelectorAll('.play');
-    let botaoPause = document.querySelectorAll('.pause');
-    let botaoReset = document.querySelectorAll('.reset');
+    let botaoPlay = document.getElementById('play');
+    let botaoPause = document.getElementById('pause');
+    let botaoReset = document.getElementById('reset');
 
-    let hora = document.getElementsByClassName('hora');
-    let min = document.getElementsByClassName('min');
-    let seg = document.getElementsByClassName('seg');
+    let hora = document.getElementById('hora');
+    let min = document.getElementById('min');
+    let seg = document.getElementById('seg');
 
     let segAtual = 0;
     let minAtual = 0;
     let horaAtual = 0;
 
     botaoPlay.addEventListener("click", () => {
-        var intervalo = setInterval(cronometro, 1000);
+        if (cronometroSelecionado == 'normal') {
+            intervalo = setInterval(cronometro, 1000);
+        } else {
+            if (pomodoroSelecionado == 'concentracao') {
+                intervalo = setInterval(pomodoroConcentracao, 1000);
+            } else {
+                intervalo = setInterval(pomodoroDescanso, 1000);
+            }
+        }
         botaoPlay.disabled = true;
         botaoPause.disabled = false;
         botaoReset.disabled = false;
@@ -71,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function cronometro() {
         if (segAtual <= 60) {
-            if (segAtual < 9) {
+            if (segAtual <= 9) {
                 segAtual++;
                 seg.innerHTML = "0" + segAtual;
             } else {
@@ -83,7 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 segAtual = 0;
                 seg.innerHTML = "00";
                 if (minAtual <= 60) {
-                    if (minAtual < 9) {
+                    if (minAtual <= 9) {
                         minAtual++;
                         min.innerHTML = "0" + minAtual;
                     } else {
@@ -95,11 +124,56 @@ document.addEventListener('DOMContentLoaded', () => {
                         minAtual = 0;
                         min.innerHTML = "00";
 
-                        if (horaAtual < 9) {
+                        if (horaAtual <= 9) {
                             hora.innerHTML = "0" + horaAtual;
                         }
                     }
                 }
+            }
+        }
+    }
+
+    function pomodoroConcentracao() {
+        if (segAtual > 0) {
+            segAtual--;
+            seg.innerHTML = segAtual;
+            if (segAtual <= 9) {
+                segAtual--;
+                seg.innerHTML = "0" + segAtual;
+            }
+        } else if (segAtual === 0) {
+            segAtual = 59;
+            seg.innerHTML = segAtual;
+            if (minAtual <= 25) {
+                if (minAtual <= 9) {
+                    minAtual--;
+                    min.innerHTML = "0" + minAtual;
+                } else {
+                    minAtual--;
+                    min.innerHTML = minAtual;
+                }
+            }
+        }
+    }
+
+    function pomodoroDescanso() {
+        if (segAtual > 0) {
+            console.log("1")
+            segAtual--;
+            seg.innerHTML = segAtual;
+            if (segAtual <= 9) {
+                console.log("2")
+                segAtual--;
+                seg.innerHTML = "0" + segAtual;
+            }
+        } else if (segAtual === 0) {
+            console.log("3")
+            segAtual = 59;
+            seg.innerHTML = segAtual;
+            if (minAtual <= 5) {
+                console.log("4")
+                minAtual--;
+                min.innerHTML = "0" + minAtual;
             }
         }
     }
